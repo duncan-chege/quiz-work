8cc2174b626ec30f531feabda25a49e9dbce6dcc
dunyung1 <dshege4@gmail.com>
2018-12-10 10:02:39 +0300

    add radio button validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
   $("button.btn").click(function(){
-    var methodValue=parseInt($!('input[name=method]:checked').val());
+    var methodValue=parseInt($('input[name=method]:checked').val());
     var diffValue=parseInt($('input[name=diff]:checked').val());
     var typeValue=parseInt($('input[name=type]:checked').val());
     var opValue=parseInt($('input[name=op]:checked').val());
     var arrayValue=parseInt($('input[name=array]:checked').val());
 
+    var mValidate=$("input[name=method]:checked").length;
+    var dValidate=$("input[name=diff]:checked").length;
+    var tValidate=$("input[name=type]:checked").length;
+    var oValidate=$("input[name=op]:checked").length;
+    var aValidate=$("input[name=array]:checked").length;
+
     var answer= methodValue + diffValue + typeValue + opValue + arrayValue;
-    $('.output').html("<b>Final marks are: " +answer+ "%");
+    //testing form validation using length method
+    if(mValidate<1 || dValidate<1 || tValidate<1 || oValidate<1 || aValidate<1){
+        $('.output').html("Input answers");
+    }else if(answer>0){
+      $('.output').html("<b>You have scored: " +answer+ "%</b>");
+    }else {
+      $('.output').html("<b>You have failed</b>");
+    }
 
   $(".well").show();
   });
